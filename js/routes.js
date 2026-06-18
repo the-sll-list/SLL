70ae8cbe246762e2d0518373d5fc35231876f6ea
lubiegry353 <szymonkurp3@gmail.com>
2026-06-18 15:42:03 +0200

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -3,7 +3,7 @@ import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import Submit from './pages/Submit.js';
 
-export default [export default [
+export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
